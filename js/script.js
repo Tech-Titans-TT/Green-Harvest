bb2b64f88abc145e63cfa2cf50a01cff98dfc970
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-06-12 11:19:57 +0300

    small change

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const navbar = document.querySelector('.header-nav');
+const navbar = document.querySelector(".header-nav");
 
+//navbar scroll kontrol
 let lastScrollTop = 0;
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-window.addEventListener('scroll', function() {
-    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    // Scroll down
+    navbar.style.top = "-100px";
+  } else {
+    // Scroll up
+    navbar.style.top = "32px";
+  }
 
-    if (scrollTop > lastScrollTop) {
-        // Scroll down
-        navbar.style.top = '-100px'; // Navbar'ı yukarı kaydırarak gizle
-    } else {
-        // Scroll up
-        navbar.style.top = '32px'; // Navbar'ı geri getir
-    }
-
-    lastScrollTop = scrollTop;
+  lastScrollTop = scrollTop;
 });
