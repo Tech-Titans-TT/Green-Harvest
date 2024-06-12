f2310ffb1971dd2a770cf25a8445023a2629ac4c
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-06-12 11:41:55 +0300

    js fix

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
-const navbar = document.querySelector(".header-nav");
-
-//navbar scroll kontrol
+const navbar = document.querySelector('.header-nav');
+//scroll ile navbar gizlenmesi ve aktif edilmesi
 let lastScrollTop = 0;
-window.addEventListener("scroll", function () {
-  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+window.addEventListener('scroll', function() {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-  if (scrollTop > lastScrollTop) {
-    // Scroll down
-    navbar.style.top = "-100px";
-  } else {
-    // Scroll up
-    navbar.style.top = "32px";
-  }
-
-  lastScrollTop = scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Scroll down kismi
+        navbar.style.top = '-100px';
+    } else {
+        //scroll up kismi
+        navbar.style.top = '32px';
+    }
+    lastScrollTop = scrollTop;
 });
