8f40175660078e1aa6abd96ed214cf1fcfaede24
SubatYucel <subat.yucel1@gmail.com>
2024-06-19 13:13:26 +0300

    base added to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
-  root: 'src',
+  root: "src",
   build: {
     rollupOptions: {
-      input: glob.sync('./src/*.html'),
+      input: glob.sync("./src/*.html"),
     },
-    outDir: '../dist',
+    outDir: "../dist",
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+  base: "/GreenHarvest",
 });
